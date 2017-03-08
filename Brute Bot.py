48c36ca1e547f2d698c2e062a443e0c250e22a5c
Drazard <antdarlos@gmail.com>
2017-03-08 17:57:16 +1030

    Update Brute Bot.py

--- Brute Bot.py
+++ Brute Bot.py
@@ -16,18 +16,18 @@ for line in f:
     if "your cracker is not good enough." in result:
         print("Cracker not good enough!")
         f = open("cantcrack.txt","a")
-        f.write(line+"\n")
+        f.write(line+" \n")
             
     elif "This IP is already on your hacked database" in result:
         print(line+" Already hacked!")
         f = open("alreadyhacked.txt", "a")
-        f.write(line+"\n")
+        f.write(line+" \n")
         
 
     elif "This ip does not exists." in result:
         print(line+" Does not exist!")
         f = open("doesnotexist.txt", "a")
-        f.write(line+"\n")
+        f.write(line+" \n")
 
     else:
 
